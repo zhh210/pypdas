13e93a5db752ee3d2d21b269294ff1acc9be6cef
zhh210 <zhh210@lehigh.edu>
2014-02-24 00:02:41 -0500

    Revised the rand example to compare exact and inexact ssm.

--- examples/eg_rand.py
+++ examples/eg_rand.py
@@ -17,35 +17,34 @@ pdmat = sprandsym(size=5,cond=100,sp=0.5)
 print(pdmat)
 
 # Generate a random QP with 100 variables, 1 equality, 1 inequaity
-qp1 = randQP(size = 100, numeq = 1, numineq = 1)
-qp2 = randQP(size = 100, numeq = 1, numineq = 1)
+qp = randQP(size = 100, numeq = 1, numineq = 1)
 
 # Generate a random bound constrained QP with 100 variables
-bqp1 = randBQP(size = 100) 
+bqp = randBQP(size = 100) 
 
 # Another way to generate a random bound constrained QP with 100 variables
 bqp2 = randQP(size = 100, numeq = 0, numineq = 0) 
 
 
 # Solve the random bound constrained QP by PDAS with exact solve
-pdas = PDAS(bqp1)
+pdas = PDAS(bqp)
 print 'Solving random bound-constrained QP with exact subproblem solve:'
 pdas.exact_solve()
 
 # Solve the random bound constrained QP by PDAS with inexact solve
 # import pdb
 # pdb.set_trace()
-pdas2 = PDAS(bqp2)
+pdas2 = PDAS(bqp)
 print 'Solving random bound-constrained QP with inexact subproblem solve:'
 pdas2.inexact_solve()
 
 
 # Solve the random bound constrained QP by PDAS with exact solve
-pdas3 = PDAS(qp1)
+pdas3 = PDAS(qp)
 print 'Solving random constrained QP with exact subproblem solve:'
 pdas3.exact_solve()
 
 # Solve the random bound constrained QP by PDAS with inexact solve
-pdas4 = PDAS(qp2)
+pdas4 = PDAS(qp)
 print 'Solving random constrained QP with inexact subproblem solve:'
 pdas4.inexact_solve()
